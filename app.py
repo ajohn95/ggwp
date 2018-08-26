135e9ddc79edffb4cf99d8701e8ec72c8e40b797
ElecTreeFrying <johnjamesermitano@yahoo.com>
2018-08-26 09:57:49 +0800

    init commit

---
+++ app.py
@@ -0,0 +1,29 @@
+from threading import Timer;
+import os
+import csv
+import csv;
+import codecs;
+
+
+def init():
+    print('');
+    print('*******************************');
+    print(' S i m p l e  L o g i n  A p p');
+    print('*******************************');
+    username = input('Press (a) to create an account, (b) to login.\n>>> ');
+
+    if username == 'a':
+        signup();
+    elif username == 'b':
+        login();
+    else:
+        clear = lambda: os.system('cls');
+        clear();
+        init();
+
+def log():
+    toReturn();
+    time = Timer(1.0, init);
+    time.start();
+
+log();
